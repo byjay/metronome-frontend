e3e6d1357055ee3a953c54f317ca8733c6fc1070
byjay <rururu0729@gmail.com>
2020-11-22 17:43:26 +0900

    Full grid layout

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -4,10 +4,11 @@ import styled from 'styled-components'
 export const Layout = styled.section`
   display: grid;
   width: 100%;
+  height: 100vh;
   grid-template-areas: "nav nav"
                        "sidebar  content";
  grid-template-columns: 150px 1fr;
- grid-template-rows: 50px 100%;
+ grid-template-rows: 50px 1fr;
 `
 
 export const Navigation = styled.nav`
