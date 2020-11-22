d70b83b6119b36000fcb152f5b9b00d72dd48239
byjay <rururu0729@gmail.com>
2020-11-22 15:09:42 +0900

    focus Editor

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -3,22 +3,22 @@ import {Editor, EditorState} from "draft-js";
 import "draft-js/dist/Draft.css";
 
 export default function MyEditor() {
-    const [editorState, setEditorState] = React.useState(() =>
+    const [editorState, setEditorState] = React.useState<EditorState>(() =>
         EditorState.createEmpty()
     );
 
+
     const editor = React.useRef(null);
 
-    function focusEditor() {
-        if (editor.current !== null) {
-            // editor.current.focus();
-        }
+    function onClickBox() {
+        // @ts-ignore
+        editor.current.focus()
     }
 
     return (
         <div
             style={{border: "1px solid black", minHeight: "6em", cursor: "text"}}
-            onClick={focusEditor}
+            onClick={onClickBox}
         >
             <Editor
                 ref={editor}
